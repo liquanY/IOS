8047d14bddfc2feb7629a37ae6da6c906c91c925
ziyuxin123123 <176290392@qq.com>
2017-05-09 20:30:35 +0800

    01_c

---
+++ 01_c/01_c/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  01_c
+//
+//  Created by admin on 17/5/9.
+//  Copyright © 2017年 中信网络科技. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
